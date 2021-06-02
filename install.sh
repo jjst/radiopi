e13cef6f3cb6cc503208ac3c09fe687f09f32cd0
Jeremie Jost <jeremie.jost@zalando.ie>
2021-06-02 16:12:44 +0200

    Fix install script

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sudo ln -sf ./radio.py /usr/bin/radiopi
+sudo ln -sf $PWD/radio.py /usr/bin/radiopi
 sudo cp ./install/systemd/radiopi.service /etc/systemd/system/radiopi.service
 sudo chmod 644 /etc/systemd/system/radiopi.service
 sudo systemctl enable radiopi
