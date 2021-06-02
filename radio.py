82b8db3871d9e82166ba89fe96997983b91068e1
Jeremie Jost <jeremie.jost@zalando.ie>
2021-06-02 17:52:19 +0200

    Retry when loading stream fails

--- radio.py
+++ radio.py
@@ -1,6 +1,25 @@
 #!/usr/bin/env python3
 
 import subprocess
+import time
+import sys
+
+DEFAULT_STREAM = "http://radiomeuh.ice.infomaniak.ch/radiomeuh-128.mp3"
+delay = 1
+
+def vlc_main_loop(stream_url):
+    # Consider using the `backoff` library for better exponential retry delay
+    while True:
+        print(f"Loading stream: {stream_url}")
+        subprocess.run(["cvlc", stream, "vlc://quit"])
+        print("Failed to open stream. Relaunching VLC after delay.")
+        time.sleep(delay)
+
 
 if __name__ == '__main__':
-    subprocess.run(["cvlc", "http://radiomeuh.ice.infomaniak.ch/radiomeuh-128.mp3"])
+    try:
+        stream = sys.argv[1]
+    except IndexError:
+        stream = DEFAULT_STREAM
+
+    vlc_main_loop(stream)
