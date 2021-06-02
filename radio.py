1a3cd08686eb1f6de122c5cf9ff3a3fe18c598a0
Jeremie Jost <jeremie.jost@zalando.ie>
2021-06-02 19:04:40 +0200

    Implement history

--- radio.py
+++ radio.py
@@ -3,9 +3,36 @@
 import subprocess
 import time
 import sys
+import os
+import shutil
+import tempfile
 
+CONFIG_PATH = os.path.expanduser("~/.config/radiopi")
+STREAM_HISTORY_PATH = os.path.join(CONFIG_PATH, "history")
 DEFAULT_STREAM = "http://radiomeuh.ice.infomaniak.ch/radiomeuh-128.mp3"
 delay = 1
+
+def init_config():
+    if not os.path.exists(CONFIG_PATH):
+        os.makedirs(CONFIG_PATH)
+
+def load_stream_history():
+    try:
+        with open(STREAM_HISTORY_PATH, 'r') as history_file:
+            lines = history_file.read().splitlines()
+            return lines
+    except FileNotFoundError:
+        return []
+
+def update_stream_history(stream, history):
+    if not history or stream != history[0]:
+        new_history = [stream] + history
+        with tempfile.TemporaryDirectory() as tempdir:
+            new_history_file_path = os.path.join(tempdir, 'history')
+            with open(new_history_file_path, 'w') as new_history_file:
+                new_history_file.write('\n'.join(new_history))
+            shutil.move(new_history_file_path, STREAM_HISTORY_PATH)
+
 
 def vlc_main_loop(stream_url):
     # Consider using the `backoff` library for better exponential retry delay
@@ -17,9 +44,15 @@ def vlc_main_loop(stream_url):
 
 
 if __name__ == '__main__':
+    init_config()
+    stream_history = load_stream_history()
     try:
         stream = sys.argv[1]
     except IndexError:
-        stream = DEFAULT_STREAM
+        try:
+            stream = stream_history[0]
+        except IndexError:
+            stream = DEFAULT_STREAM
 
+    update_stream_history(stream, stream_history)
     vlc_main_loop(stream)
