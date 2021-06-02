dd9d6e38d8499aa33b47b0e162a56b1b764d9fbd
Jeremie Jost <jeremie.jost@zalando.ie>
2021-06-02 19:25:02 +0200

    Add some prints to install

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 sudo pip3 install -r requirements.txt
+echo "Setting up radiopi as systemd service..."
 sudo ln -sf $PWD/radio.py /usr/bin/radiopi
 sudo cp ./install/systemd/radiopi.service /etc/systemd/system/radiopi.service
 sudo chmod 644 /etc/systemd/system/radiopi.service
 sudo systemctl enable radiopi
+echo "Install successful."
