89f66715153931c259000f5e8aefcfce835e1f50
Jeremie Jost <jeremie.jost@zalando.ie>
2021-06-02 19:21:15 +0200

    Install python dependencies as part of install script

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+sudo pip3 install -r requirements.txt
 sudo ln -sf $PWD/radio.py /usr/bin/radiopi
 sudo cp ./install/systemd/radiopi.service /etc/systemd/system/radiopi.service
 sudo chmod 644 /etc/systemd/system/radiopi.service
