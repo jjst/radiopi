abc900131b5fef611f00dddce7980481acf59def
Jeremie Jost <jeremie.jost@zalando.ie>
2021-06-02 16:35:29 +0200

    Use cvlc

--- radio.py
+++ radio.py
@@ -3,4 +3,4 @@
 import subprocess
 
 if __name__ == '__main__':
-    subprocess.run(["vlc", "http://radiomeuh.ice.infomaniak.ch/radiomeuh-128.mp3"])
+    subprocess.run(["cvlc", "http://radiomeuh.ice.infomaniak.ch/radiomeuh-128.mp3"])
