729c386b08c9c75d5fcc9e094ba7d61c70768da5
Jeremie Jost <jeremie.jost@zalando.ie>
2021-06-04 20:41:04 +0200

    Add pause

--- button.py
+++ button.py
@@ -1,7 +1,9 @@
 from gpiozero import Button
+from signal import pause
 
 button = Button("GPIO17")
 
 button.when_pressed = lambda: print("Pressed")
 button.when_released = lambda: print("Released")
 
+pause()
