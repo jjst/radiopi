0efd5f68aac73291ea1d9719c9c690371b671893
Jeremie Jost <jeremie.jost@zalando.ie>
2021-06-05 20:42:12 +0200

    DCE

--- button.py
+++
@@ -1,9 +0,0 @@
-from gpiozero import Button
-from signal import pause
-
-button = Button("GPIO17")
-
-button.when_pressed = lambda: print("Pressed")
-button.when_released = lambda: print("Released")
-
-pause()
