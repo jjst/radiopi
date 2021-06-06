96d212a4b52c92ab80757fbd76f8d9ed61655688
Jeremie Jost <jeremie.jost@zalando.ie>
2021-06-06 13:34:54 +0200

    Add sample e-paper display code

---
+++ epd_2in13_V2_test.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+import sys
+import os
+libdir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'lib')
+if os.path.exists(libdir):
+    sys.path.append(libdir)
+
+import logging
+from waveshare_epd import epd2in13_V2
+import time
+from PIL import Image,ImageDraw,ImageFont
+import traceback
+
+logging.basicConfig(level=logging.DEBUG)
+
+try:
+    logging.info("epd2in13_V2 Demo")
+
+    epd = epd2in13_V2.EPD()
+    logging.info("init and Clear")
+    epd.init(epd.FULL_UPDATE)
+    epd.Clear(0xFF)
+
+    # Drawing on the image
+    font24 = ImageFont.truetype('DejaVuSans.ttc', 24)
+
+    logging.info("1.Drawing on the image...")
+    image = Image.new('1', (epd.height, epd.width), 255)  # 255: clear the frame
+    draw = ImageDraw.Draw(image)
+
+
+    # # partial update
+    logging.info("4.show time...")
+    time_image = Image.new('1', (epd.height, epd.width), 255)
+    time_draw = ImageDraw.Draw(time_image)
+
+    epd.init(epd.FULL_UPDATE)
+    epd.displayPartBaseImage(epd.getbuffer(time_image))
+
+    epd.init(epd.PART_UPDATE)
+    num = 0
+    while (True):
+        time_draw.rectangle((120, 80, 220, 105), fill = 255)
+        time_draw.text((120, 80), time.strftime('%H:%M:%S'), font = font24, fill = 0)
+        epd.displayPartial(epd.getbuffer(time_image))
+        num = num + 1
+        if(num == 10):
+            break
+    # epd.Clear(0xFF)
+    logging.info("Clear...")
+    epd.init(epd.FULL_UPDATE)
+    epd.Clear(0xFF)
+
+    logging.info("Goto Sleep...")
+    epd.sleep()
+
+except IOError as e:
+    logging.info(e)
+
+except KeyboardInterrupt:
+    logging.info("ctrl + c:")
+    epd2in13_V2.epdconfig.module_exit()
+    exit()
