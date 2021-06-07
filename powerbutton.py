c8048b07b6d3f909e2a89130cfdbac1812874fb5
Jeremie Jost <jeremiejost@gmail.com>
2021-06-07 16:45:44 +0200

    remove button debug code, giving up

--- powerbutton.py
+++ powerbutton.py
@@ -1,6 +1,4 @@
 import gpiozero
-import datetime
-import time
 from gpiozero import Button
 
 
@@ -11,8 +9,6 @@ SHORT_PRESS_SECONDS = 1
 class PowerButton():
 
     def __init__(self, player, pin=BUTTON_GPIO_PIN):
-        self.last_time_pressed = None
-        self.last_time_released = None
         self.button = Button(BUTTON_GPIO_PIN)
         self.player = player
         if self.button.is_pressed:
@@ -22,16 +18,6 @@ class PowerButton():
 
     def _when_released(self):
         self.player.stop()
-        self.last_time_released = time.monotonic()
 
     def _when_pressed(self):
-        time_pressed = time.monotonic()
-        if self.last_time_pressed:
-            delta = time_pressed - self.last_time_released
-            print(self.last_time_released)
-            print(time_pressed)
-            print(delta)
-            if delta < SHORT_PRESS_SECONDS:
-                print("Detected a short press")
-        self.last_time_pressed = time_pressed
         self.player.start()
